aff0976a7955a7c9871f48b3cd7bd31dd65fb218
DIDRICH Damien <89045920+skunkoff1@users.noreply.github.com>
2022-02-13 18:24:28 +0100

    resize/ drag/ write and z-index ok

--- ressources/js/script.js
+++ ressources/js/script.js
@@ -1,38 +1,40 @@
-let postItArray = [];
-let drag = false;
-let write = true;
+let postItArray = []; // Tableau de tous les post-it
+let dragMode = true; // Flag bouton deplaver post-it
+let resizeMode = false; // Flag bouton redimensionner post-it
+let count = 0;
 
-let dragButton = document.getElementById('dragButton');
-let writeButton = document.getElementById('writeButton');
+// Récupération des boutons
+let buttonDrag = document.getElementById('modeButtonDrag');
+let buttonResize = document.getElementById('modeButtonResize');
 
-dragButton.style.backgroundColor = "red";
-dragButton.style.color = "white";
-writeButton.style.height = "80px";
-writeButton.style.width = "200px";
-writeButton.style.fontSize = "25px";
+// Style par défaut des boutons -> mode par défaut == deplacer
+
+buttonResize.style.height = "40px";
+buttonResize.style.width = "150px";
+buttonResize.style.backgroundColor = "rgb(226, 48, 48)";
+
+// Class Post-it
 class Postit {
-    width;
-    height;
-    zIndex;
-    posX;
-    posY;
-    isDraging;
-    post;
-    xOnStart;
-    yOnStart;
-    resize;
-    isResizing;
+    width; // Largeur à l'écran
+    height; // Hauteur à l'écran
+    zIndex; // Son z-index
+    posX; // Position horizontale à l'écran
+    posY; // Position verticale à l'écran
+    isDraging; // Flag déplavement enclenché
+    isResizing; // Flag redimensionnement enclenché
+    post; // la div principale du post-it
+    resize; // la div pour le resize (coin bas droit du post-it)
+    xOnStart; // sa position horizontal de départ pour calculer le déplacement
+    yOnStart; // sa position verticale de départ pour calculer le déplacement
 
     constructor(x, y, width, height) {
         this.posX = x;
         this.posY = y;
         this.width = width;
         this.height = height;
-        this.zIndex = postItArray.length + 1;
         this.isDraging = false;
         this.isResizing = false;
-        this.post = document.createElement('div');
-        this.resize = document.createElement('div');
+        this.post = document.createElement('textarea');
         this.createPostIt();
     }
 
@@ -44,81 +46,48 @@ class Postit {
         this.post.style.height = this.height + "px";
         this.post.style.left = this.posX + "px";
         this.post.style.top = this.posY + "px";
-        this.post.style.zIndex = this.zIndex;
-        this.resize.className = "resize";
-        this.resize.style.left = parseInt(this.width - 20) + "px";
-        this.resize.style.top = parseInt(this.height - 20) + "px";
         postItArray.push(this.post);
-        this.post.appendChild(this.resize);
+        this.zIndex = postItArray.length+count;
+        this.post.style.zIndex = this.zIndex;
         board.appendChild(this.post);
         this.post.addEventListener('mousedown', this.startDrag.bind(this));
         this.post.addEventListener('mouseup', this.stopDrag.bind(this));
         this.post.addEventListener('mousemove', this.drag.bind(this));
-        this.resize.addEventListener('mousedown', this.startResize.bind(this));
-        this.resize.addEventListener('mouseup', this.stopResize.bind(this));
-        this.resize.addEventListener('mousemove', this.resizing.bind(this));
     }
 
     startDrag(e) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        if (drag === true) {
+        // e.preventDefault();
+        // e.stopImmediatePropagation();
+        if (dragMode === true) {
             this.isDraging = true;
-            this.post.style.zIndex += 1000;
+            this.post.style.zIndex = "1000";
             this.xOnStart = e.clientX + window.pageXOffset;
             this.yOnStart = e.clientY + window.pageYOffset;
         }
 
     }
     stopDrag(e) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        if (drag === true) {
+        // e.preventDefault();
+        // e.stopImmediatePropagation();
+        if (dragMode === true) {
+            count++;
             this.isDraging = false;
-            this.post.style.zIndex -= 1000;
+            this.post.style.zIndex = postItArray.length+count;
             this.posX = this.post.style.left;
             this.posY = this.post.style.top;
         }
     }
     drag(e) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        if (this.isDraging === true && drag === true) {
+        // e.preventDefault();
+        // e.stopImmediatePropagation();
+        if (this.isDraging === true && dragMode === true) {
             this.post.style.left = parseInt(e.clientX) + parseInt(this.posX) - this.xOnStart + "px";
             this.post.style.top = parseInt(e.clientY) + parseInt(this.posY) - this.yOnStart + "px";
         }
-    }
-    startResize(e) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        this.isResizing = true;
-        this.post.style.zIndex += 1000;
-        this.xOnStart = e.clientX + window.pageXOffset;
-        this.yOnStart = e.clientY + window.pageYOffset;
-    }
-    stopResize(e) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        this.isResizing = false;
-        this.width = this.post.style.width;
-        this.height = this.post.style.height;
-        this.post.style.zIndex -= 1000;
-    }
-    resizing(e) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        if (this.isResizing === true) {
-            this.post.style.width = parseInt(e.clientX) + parseInt(this.width) - this.xOnStart + "px";
-            this.post.style.height = parseInt(e.clientY) + parseInt(this.height) - this.yOnStart + "px";
-            this.resize.style.left = parseInt(this.post.style.width) - 20 + "px";
-            this.resize.style.top = parseInt(this.post.style.height) - 20 + "px";
-        }
-    }
+    }    
 }
-let post1 = new Postit(400, 400, 150, 150);
-let post2 = new Postit(200, 400, 100, 100);
 
-document.onmousemove = function(e) {
+document.onmousemove = function (e) {
     let x = document.querySelector('.horizontal');
     let y = document.querySelector('.vertical');
     x.innerHTML = e.clientX + window.pageXOffset;
@@ -126,35 +95,39 @@ document.onmousemove = function(e) {
 
 };
 
-function canDrag() {
-    drag = true;
-    write = false;
-    writeButton.style.backgroundColor = "red";
-    writeButton.style.color = "white";
-    writeButton.style.height = "60px";
-    writeButton.style.width = "150px";
-    writeButton.style.fontSize = "16px";
-    dragButton.style.backgroundColor = "green";
-    dragButton.style.color = "black";
-    dragButton.style.height = "80px";
-    dragButton.style.width = "200px";
-    dragButton.style.fontSize = "22px";
+function mode(mode) {    
+    if (mode === 'drag') {
+        dragMode = true;
+        resizeMode = false;
+        buttonDrag.style.height = "60px";
+        buttonDrag.style.width = "180px";
+        buttonDrag.style.backgroundColor = "rgb(18, 187, 18)";
+        buttonResize.style.height = "40px";
+        buttonResize.style.width = "150px";
+        buttonResize.style.backgroundColor = "rgb(226, 48, 48)";
+        postItArray.forEach((item) => {
+            item.style.resize = "none";
+        })
+    }
+    if (mode === 'resize') {
+        dragMode = false;
+        resizeMode = true;
+        buttonDrag.style.height = "40px";
+        buttonDrag.style.width = "150px";
+        buttonDrag.style.backgroundColor = "rgb(226, 48, 48)";
+        buttonResize.style.height = "60px";
+        buttonResize.style.width = "180px";
+        buttonResize.style.backgroundColor = "rgb(18, 187, 18)";
+        postItArray.forEach((item) => {
+            item.style.resize = "both";
+        })
+    }
 }
 
-function canWrite() {
-    drag = false;
-    write = true;
-    dragButton.style.backgroundColor = "red";
-    dragButton.style.color = "white";
-    dragButton.style.height = "60px";
-    dragButton.style.width = "150px";
-    dragButton.style.fontSize = "16px";
-    writeButton.style.backgroundColor = "green";
-    writeButton.style.color = "black";
-    writeButton.style.height = "80px";
-    writeButton.style.width = "200px";
-    writeButton.style.fontSize = "22px";
+function addPostIt() {
+    let post = new Postit(500,400,150,150);
 }
+
 
 /**********************************************************************************/
 /****************** FAIRE UN FRIGO CE SERA VACHEMENT PLUS STYLE *******************/
