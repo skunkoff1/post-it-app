2dae23bf014c49146c27705699afc3b11abee813
DIDRICH Damien <89045920+skunkoff1@users.noreply.github.com>
2022-02-13 16:45:40 +0100

    add button write/drag

--- ressources/js/script.js
+++ ressources/js/script.js
@@ -1,5 +1,15 @@
 let postItArray = [];
+let drag = false;
+let write = true;
 
+let dragButton = document.getElementById('dragButton');
+let writeButton = document.getElementById('writeButton');
+
+dragButton.style.backgroundColor = "red";
+dragButton.style.color = "white";
+writeButton.style.height = "80px";
+writeButton.style.width = "200px";
+writeButton.style.fontSize = "25px";
 class Postit {
     width;
     height;
@@ -52,24 +62,28 @@ class Postit {
     startDrag(e) {
         e.preventDefault();
         e.stopImmediatePropagation();
-        this.isDraging = true;
-        this.post.style.zIndex += 1000;
-        this.xOnStart = e.clientX + window.pageXOffset;
-        this.yOnStart = e.clientY + window.pageYOffset;
+        if (drag === true) {
+            this.isDraging = true;
+            this.post.style.zIndex += 1000;
+            this.xOnStart = e.clientX + window.pageXOffset;
+            this.yOnStart = e.clientY + window.pageYOffset;
+        }
+
     }
     stopDrag(e) {
         e.preventDefault();
         e.stopImmediatePropagation();
-        this.isDraging = false;
-        this.posX = this.post.style.left;
-        this.posY = this.post.style.top;
-        this.post.style.zIndex -= 1000;
-        console.log(this)
+        if (drag === true) {
+            this.isDraging = false;
+            this.post.style.zIndex -= 1000;
+            this.posX = this.post.style.left;
+            this.posY = this.post.style.top;
+        }
     }
     drag(e) {
         e.preventDefault();
         e.stopImmediatePropagation();
-        if (this.isDraging == true) {
+        if (this.isDraging === true && drag === true) {
             this.post.style.left = parseInt(e.clientX) + parseInt(this.posX) - this.xOnStart + "px";
             this.post.style.top = parseInt(e.clientY) + parseInt(this.posY) - this.yOnStart + "px";
         }
@@ -93,21 +107,18 @@ class Postit {
     resizing(e) {
         e.preventDefault();
         e.stopImmediatePropagation();
-        if (this.isResizing == true) {
+        if (this.isResizing === true) {
             this.post.style.width = parseInt(e.clientX) + parseInt(this.width) - this.xOnStart + "px";
             this.post.style.height = parseInt(e.clientY) + parseInt(this.height) - this.yOnStart + "px";
             this.resize.style.left = parseInt(this.post.style.width) - 20 + "px";
             this.resize.style.top = parseInt(this.post.style.height) - 20 + "px";
-            console.log(this.post.style.width)
         }
     }
 }
 let post1 = new Postit(400, 400, 150, 150);
 let post2 = new Postit(200, 400, 100, 100);
 
-
-
-document.onmousemove = function (e) {
+document.onmousemove = function(e) {
     let x = document.querySelector('.horizontal');
     let y = document.querySelector('.vertical');
     x.innerHTML = e.clientX + window.pageXOffset;
@@ -115,6 +126,35 @@ document.onmousemove = function (e) {
 
 };
 
+function canDrag() {
+    drag = true;
+    write = false;
+    writeButton.style.backgroundColor = "red";
+    writeButton.style.color = "white";
+    writeButton.style.height = "60px";
+    writeButton.style.width = "150px";
+    writeButton.style.fontSize = "16px";
+    dragButton.style.backgroundColor = "green";
+    dragButton.style.color = "black";
+    dragButton.style.height = "80px";
+    dragButton.style.width = "200px";
+    dragButton.style.fontSize = "22px";
+}
+
+function canWrite() {
+    drag = false;
+    write = true;
+    dragButton.style.backgroundColor = "red";
+    dragButton.style.color = "white";
+    dragButton.style.height = "60px";
+    dragButton.style.width = "150px";
+    dragButton.style.fontSize = "16px";
+    writeButton.style.backgroundColor = "green";
+    writeButton.style.color = "black";
+    writeButton.style.height = "80px";
+    writeButton.style.width = "200px";
+    writeButton.style.fontSize = "22px";
+}
 
 /**********************************************************************************/
 /****************** FAIRE UN FRIGO CE SERA VACHEMENT PLUS STYLE *******************/
